d0f969fd0df96d04f584b2d256bd3425867f179d
mostafa-kamal <mmkamal021@gmail.com>
2023-04-04 16:19:59 +0600

    commited

---
+++ esty.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.etsy.com/shop/BellaBerryDesigns'
+page = requests.get(url)
+# print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup)
+
+# pagination = soup.find('ul', class_='wt-action-group wt-list-inline wt-flex-no-wrap  wt-flex-no-wrap wt-pt-xl-2 wt-pb-xl-4')
+pagination = soup.find('nav', attrs={'aria-label':'Pagination of listings'})
+# print(pagination)
+
+page_num = pagination.find_all('li')
+page_numbers = page_num.find_all('span')
+for i in range(0, len(page_numbers)):
+ if page_numbers[i].isDigit():
+  print(page_numbers[i])
+# for page in page_numbers:
+#     no = page.find('a', class_="wt-action-group__item").find('span', class_="wt-screen-reader-only")
+#     print(no)
+    # print(no.strip())
+    # if no.strip().isdigit():
+    #     products_url = "https://www.etsy.com/shop/BellaBerryDesigns?page="+no.strip()+#items
