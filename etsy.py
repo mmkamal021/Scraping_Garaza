d54e88468f921642c57f7367abe1d5fdea78913f
Adnan Ferdous Ashrafi <nazib91@gmail.com>
2023-04-05 22:49:11 +0600

    etsy pagination

---
+++ etsy.py
@@ -0,0 +1,52 @@
+# Need to information title, description, variant option with price, price, category (All products) 
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.etsy.com/shop/BellaBerryDesigns'
+page = requests.get(url)
+# print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+# print(soup)
+
+# pagination = soup.find('ul', class_='wt-action-group wt-list-inline wt-flex-no-wrap  wt-flex-no-wrap wt-pt-xl-2 wt-pb-xl-4')
+
+pagi = soup.find_all("div", class_="wt-show-xl")
+# print(pagi)
+for page in pagi:
+    if page.find("nav", attrs={"aria-label":"Pagination of listings"}):
+        nav_attrs = page.find("nav", attrs={"aria-label":"Pagination of listings"})
+        # print(nav_attrs)
+        spans = nav_attrs.find_all('span')
+        for i in range(3, len(spans)-1, 2):
+            print(spans[i].text.strip())
+            
+# pagination = soup.find('nav', attrs={'aria-label':'Pagination of listings'})
+# print(pagination)
+
+# page_num = pagination.find_all('li')
+# page_numbers = page_num.find_all('span')
+# for i in range(0, len(page_numbers)):
+#  if page_numbers[i].isDigit():
+#   print(page_numbers[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for page in page_numbers:
+#     no = page.find('a', class_="wt-action-group__item").find('span', class_="wt-screen-reader-only")
+#     print(no)
+    # print(no.strip())
+    # if no.strip().isdigit():
+    #     products_url = "https://www.etsy.com/shop/BellaBerryDesigns?page="+no.strip()+#items
